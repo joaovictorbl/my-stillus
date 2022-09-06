6a71cb3a7d62a898dd0da2eae17629535ec95061
João Victor <joaovictor_bl@outlook.com>
2022-09-06 00:45:32 -0300

    testando a primeira class de dominio

--- src/main/java/br/com/mytillus/resources/CategoriaResource.java
+++ src/main/java/br/com/mytillus/resources/CategoriaResource.java
@@ -1,16 +1,25 @@
 package br.com.mytillus.resources;
 
+import br.com.mytillus.domain.Categoria;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
     @GetMapping
-    public String listar(){
-        return "endpoint ok !!";
+    public List<Categoria> listar(){
+        Categoria cat1 = new Categoria(1,"camisa");
+        Categoria cat2 = new Categoria(2,"calça");
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+        return lista;
     }
 
 }
